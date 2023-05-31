3c5c84ffebb5f63249ba016f208b5c3bedf73f7d
Paula <paula8ordonez@gmail.com>
2023-05-31 18:21:53 -0500

    login

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,22 @@ import {createUser, checkUser} from './firebase.js'
 window.addEventListener('DOMContentLoaded',()=>{
 })
 
+const startBtn = document.querySelector('.buttonStart')
+const registerBtn = document.querySelector('.buttonRegister')
+
+const seccionLandingpage = document.querySelector('.seccionLandingpage')
+const seccionLogin = document.querySelector('.login')
+const seccionRegister = document.querySelector('.register')
+
+
+
+startBtn.addEventListener('click',(e)=>{
+    seccionLandingpage.classList.add('hidden')
+    seccionLogin.classList.remove('hidden')
+
+})
+
+registerBtn.addEventListener('click',(e)=>{
+    seccionLandingpage.classList.add('hidden')
+    seccionRegister.classList.remove('hidden')
+})
