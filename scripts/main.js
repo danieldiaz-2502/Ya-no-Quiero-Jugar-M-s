59114558b93480915cfc461011c86f27b4315706
Paula <paula8ordonez@gmail.com>
2023-06-01 14:00:01 -0500

    subido el commit

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const cap = document.querySelector('.cap')
+const startCap = document.querySelector('.startCap')
+
+
+
+startCap.addEventListener('click',(e)=>{
+    location.href = "cap1.html"
+
+})
