37d01c995f18a9e32e89a8bc54091d940161e3cc
Paula <paula8ordonez@gmail.com>
2023-06-01 16:29:26 -0500

    cambios

---
+++ scripts/cap1.js
@@ -0,0 +1,10 @@
+
+const btnNext = document.querySelector('.next')
+const page= document.querySelector('.comic--page')
+
+
+btnNext.addEventListener('click',(e)=>{
+
+    page.style.backgroung
+
+})
